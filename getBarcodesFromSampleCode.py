1a7cb63f8954ed40efc6085457ffc87db9d199db
Daniel Burkhardt <Dan@nat-tvwna-inside-visitornet101-a-13720.princeton.org>
2015-02-05 15:07:46 -0500

    fixed bug regex matching 'C's in te barcode seq

--- getBarcodesFromSampleCode.py
+++ getBarcodesFromSampleCode.py
@@ -2,9 +2,8 @@
 import re,sys,argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("barcode_file",dest='filename',help="the barcode file")
+parser.add_argument("barcode_file",help="the barcode file")
 parser.add_argument("samples", nargs='+',help="a list of codes of the form PHO (Prospect hill Heated Organic)")
-
 
 #currently uses "/home/dan/barcode_table.tsv"
 def load_barcodeFile(filename):
@@ -21,19 +20,19 @@ def load_barcodeFile(filename):
 	return barcode_list
 
 #take a string ID like "HOB" or "PO" and returns the barcodes for all the samples matching those
-def get_barcodes( c ):
+def get_barcodes( c , barcode_list ):
 	subset_codes = list(c)
 	subset_ids = barcode_list
 	for code in subset_codes:
-		subset_ids = [record for record in subset_ids for entry in record if re.search(code,entry)]
+		subset_ids = [record for record in subset_ids for entry in record if re.search(code,entry) and not re.search('[ACTG]{6}',entry)]
 	return [record[0] for record in subset_ids]
 
 def main():
 	args = parser.parse_args()
-	barcode_list = load_barcodeFile(args.filename)
+	barcode_list = load_barcodeFile(args.barcode_file)
 	for subset in args.samples:
 		print "Barodes for subset %s" % subset
-		print get_barcodes(subset)
+		print get_barcodes(subset, barcode_list)
 
 if __name__ == "__main__":
 	main()
