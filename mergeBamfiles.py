d66947062c11d812bb82da6870c4126dfb32e528
dburkhardt <burkhardt.d.b@gmail.com>
2015-02-10 17:53:48 -0500

    Added concoct and GroopM functionality, cleaned up Main

--- mergeBamfiles.py
+++ mergeBamfiles.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python
-import re,sys,argparse,subprocess,os
+import re,sys,argparse,subprocess,os,multiprocessing
 
 parser = argparse.ArgumentParser()
 parser.add_argument('barcode_file',help='the barcode file')
@@ -9,7 +9,7 @@ parser.add_argument('-a','--assembly_file',dest='assembly', default='~/binning_f
 
 assembly=''
 bamdir = ''
-rundir = ''
+rundir = '' #Will eventually look like MPH_MPC_MSH
 
 #currently uses '/home/dan/barcode_table.tsv'
 def load_barcodeFile(filename):
@@ -35,48 +35,74 @@ def get_barcodes( subset , barcode_table_asList ):
 
 #returns a single Popen object running samtools merge on a set of bamfiles
 def mergeBamFilesPopen(list_of_barcodes, subset):
-	list_of_bamfiles = ['%s%s.bam' % (bamdir,barcode) for barcode in list_of_barcodes]
+        list_of_bamfiles = ['%s%s.bam' % (bamdir,barcode) for barcode in list_of_barcodes]
         if not os.path.exists('./mergedBamfiles/%s.tmp.bam' % subset): #check to see if this particular set of files has been created
                 return subprocess.Popen(['samtools', 'merge', './mergedBamfiles/%s.tmp.bam' % subset] + list_of_bamfiles)
 
-#returns a Popen object which will run the whole MetaBat pipeline
+#returns a List of Popen objects running MetaBat using the Specific Sensitive and Specific_Paired settings
 def runMetaBat(list_of_samples, list_of_merged_bamfiles):
+        print 'Binning using MetaBat...' 
         makeDepthFile(list_of_merged_bamfiles) #will block until completed
         os.system('mkdir -p ./MetaBAT/Sensitive ./MetaBAT/Specific ./MetaBAT/SpecificPair')
         try:
-        	log_sensitive = open('./MetaBAT/Sensitive/MetaBat_sensititve.log','a')
-		log_specific = open('./MetaBAT/Specific/MetaBat_specific.log','a')
-		log_specific_paired = open('./MetaBAT/SpecificPair/MetaBat_SpecificPair.log','a')
-	        p = [subprocess.Popen('nice -n 15 metabat -i %s -a MetaBat.depth.txt -o ./MetaBAT/Sensitive/bin --sensitive -v --saveTNF saved.tnf --saveDistance saved.gprob' % assembly, stdout=log_sensitive, stderr=log_sensitive, shell=True)]
+                log_sensitive = open('./MetaBAT/Sensitive/MetaBat_sensititve.log','a')
+                log_specific = open('./MetaBAT/Specific/MetaBat_specific.log','a')
+                log_specific_paired = open('./MetaBAT/SpecificPair/MetaBat_SpecificPair.log','a')
+                p = [subprocess.Popen('nice -n 15 metabat -i %s -a MetaBat.depth.txt -o ./MetaBAT/Sensitive/bin --sensitive -v --saveTNF saved.tnf --saveDistance saved.gprob' % assembly, stdout=log_sensitive, stderr=log_sensitive, shell=True)]
                 p.append(subprocess.Popen('nice -n 15 metabat -i %s -a MetaBat.depth.txt -o ./MetaBAT/Specific/bin --specific -v --saveTNF saved.tnf --saveDistance saved.gprob' % assembly, stdout=log_specific, stderr=log_specific, shell=True))
                 p.append(subprocess.Popen('nice -n 15 metabat -i %s -a MetaBat.depth.txt -p MetaBat.paired.txt -o ./MetaBAT/SpecificPair/bin --specific -v --saveTNF saved.tnf --saveDistance saved.gprob' % assembly, stdout=log_specific_paired, stderr=log_specific_paired, shell=True))
-		print 'Binning using MetaBAT...'
-		return p
+                print 'Binning using MetaBAT...'
+                return p
         except KeyboardInterrupt:
                 for a in p: a.kill()
                 sys.exit()
 
+#returns a Popen object with a single subprocess running Concoct
+def runConcoct():
+        print 'Processing depth file for Concoct'
+        os.system('mkdir -p ./Concoct')
+        subprocess.check_call("awk \'NR > 1 {for(x=1;x<=NF;x++) if(x == 1 || (x >= 4 && x % 2 == 0)) printf \"%s\", $x (x == NF || x == (NF-1) ? \"\\n\":\"\\t\")}' ./MetaBat.depth.txt > ./depth_concoct.txt", shell=True)
+        call_to_source_concoct_env = ". ./software/anaconda/envs/concoct_env/bin/activate concoct_env"
+        call_to_bin_with_concoct = "concoct --composition_file %s --coverage_file ../depth_concoct.txt" % assembly
+        concoct = subprocess.Popen('cd Concoct ; %s ; %s' % (call_to_source_concoct_env, call_to_bin_with_concoct), shell=True, executable='/bin/bash')
+        print 'Binning using CONCOCT'
+        return concoct
 
+#returns a Popen object with a single subprocess running GroopM pipeline
+def runGroopM():
+        database = "%s.gm" % rundir
+        os.system('mkdir -p ./GroopM')
+        parse = "groopm parse -t 16 %s " % database + ' '.join(list_of_bamfiles)
+        core = "groopm core %s" % database
+        extract = "groopm extract -t 32 --prefix ./GroopM/core_only/bin_groopm %s %s" % (database , assembly)
+        recruit = "groopm recruit %s" % database
+        extract_recruit = "groopm extract -t 32 --prefix ./GroopM/recruited/bin_groopm %s %s" % (database , assembly)
+        print "Binning using GroopM..."
+        return subprocess.Popen("")
+        #TODO: add log files, make sure paths checkout
+
+
+
+
+#creates a file called 'MetaBat.depth.txt' in the cwd (BLOCKING)
 def makeDepthFile(list_of_merged_bamfiles):
         try:
-		if os.path.exists('./MetaBat.depth.txt'): 
-			print 'Found MetaBat depth file. Skipping...'
-			return
-		p = subprocess.Popen([
-			'jgi_summarize_bam_contig_depths',
-			'--outputDepth',
-			'MetaBat.depth.txt',
-			'--pairedContigs',
-			'MetaBat.paired.txt'] + list_of_merged_bamfiles)
+                if os.path.exists('./MetaBat.depth.txt'): 
+                        print 'Found MetaBat depth file. Skipping...'
+                        return
+                p = subprocess.Popen([
+                        'jgi_summarize_bam_contig_depths',
+                        '--outputDepth',
+                        'MetaBat.depth.txt',
+                        '--pairedContigs',
+                        'MetaBat.paired.txt'] + list_of_merged_bamfiles)
                 p.wait()
         except KeyboardInterrupt:
                 p.kill()
                 sys.exit()
 
-
-
-
-def main():
+#parses the arguments and assigns values to the global variables
+def initializeVariables():
         args = parser.parse_args()
         barcode_table_asList = load_barcodeFile(args.barcode_file)
         global bamdir
@@ -88,37 +114,44 @@ def main():
         os.system('mkdir -p %s' % rundir)
         os.chdir('./%s' % rundir)
         print os.getcwd()
+        return args
+
+#spawns a bunch of samtools merge subprocesses and writes merged bamfiles to rundir/mergedBamfiles/ (BLOCKING)
+def mergeBamfiles(samples):
         print "Merging bamfiles..."
         os.system('mkdir -p ./mergedBamfiles')
+        list_of_bamfiles = ['./mergedBamfiles/'+sample+'.tmp.bam' for sample in samples]
         try:
-                processes = [mergeBamFilesPopen(get_barcodes(subset, barcode_table_asList),subset) for subset in args.samples]
-                if processes[0] is None: print 'Found merged bamfiles in ./%s/mergedBamfiles' % rundir
-		else:
-			for p in processes: p.wait()
-			for p in processes:
-				if p.returncode is not 0:
-					print "Merging failed"
-					sys.exit()
-				else: 
-					print "Successfully merged bamfiles!"
+                processes = [mergeBamFilesPopen(get_barcodes(subset, barcode_table_asList),subset) for subset in samples]
+                if processes[0] is None:
+                        print 'Found merged bamfiles in ./%s/mergedBamfiles' % rundir
+                        return list_of_bamfiles
+                else:
+                        for p in processes:
+                                p.wait()
+                        for p in processes:
+                                if p.returncode is not 0:
+                                        print "Merging failed"
+                                        sys.exit()
+                                else: 
+                                        print "Successfully merged bamfiles!"
+                                        return list_of_bamfiles
         except KeyboardInterrupt:
                 for p in processes: p.kill()
                 sys.exit()
-        
+
+def main():
+        args = initializeVariables()
+
         #bin using MetaBat
-        list_of_bamfiles = ['./mergedBamfiles/'+sample+'.tmp.bam' for sample in args.samples]
-        
-	metabat_processes = runMetaBat(args.samples,list_of_bamfiles)
-	print 'Binning using MetaBat...' # TODO put all the above in one large try catch block
-	#bin using Concoct
-	print 'Processing depth file for Concoct'
-	subprocess.check_call("awk 'NR > 1 {for(x=1;x<=NF;x++) if(x == 1 || (x >= 4 && x % 2 == 0)) printf "%s", $x (x == NF || x == (NF-1) ? "\n":"\t")}' ./MetaBat.depth.txt > ./depth_concoct.txt", shell=True)
-	call_to_source_concoct_env = ". ./software/anaconda/envs/concoct_env/bin/activate concoct_env"
-	call_to_bin_with_concoct = "concoct --composition_file %s --coverage_file depth_concoct.txt" % assembly
-	concoct = subprocess.Popen('%s ; %s' % (call_to_source_concoct_env, call_to_bin_with_concoct), shell=True, executable='/bin/bash')
-	print 'Binning using CONCOCT'
+        list_of_bamfiles = mergeBamfiles(args.samples)
+        metabat_process_list = runMetaBat(args.samples,list_of_bamfiles)
+        concoct_process = runConcot()
+        groopm_process = runGroopM(list_of_bamfiles)
 
         print ('done!')
+
+
 
 if __name__ == '__main__':
         main()
